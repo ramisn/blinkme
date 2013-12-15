d4c7a2471121ff6346b1449b5cea86d98794530d
Ramesh K <rameshkror@gmail.com>
2013-12-15 09:14:01 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blinkme::Application.config.secret_token = 'a7313ef51b08b2bcfaaeca3acbb17511c2ee57fa777702997e6c85be4c97621e7bf4c2f02e18dac9871028122a445968644496fe8a0ea61fab9d3533e9c3cf01'
